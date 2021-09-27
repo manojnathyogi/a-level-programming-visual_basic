63249fdec7da18af52dcd89dc180c401ba66de39
manojnathyogi <85493350+manojnathyogi@users.noreply.github.com>
2021-09-27 18:05:15 +0545

    Add files via upload

---
+++ A level vb.net/lab2/Looping statementsq.(8-14)/multiplication table of any entered number up to 10.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n, product As Integer
+        Console.WriteLine("enter th number which multiplication table you want to print")
+        n = Console.ReadLine()
+        For i = 1 To 10
+            product = n * i
+            Console.WriteLine(n & "*" & i & "=" & product)
+
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ A level vb.net/lab2/Looping statementsq.(8-14)/takes name and print (do.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim name As String
+        Dim n As Integer
+        Console.WriteLine("enter the name")
+        name = Console.ReadLine
+        n = 10
+        Do
+            Console.WriteLine(name)
+            n = n - 1
+        Loop
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
